9dd3cdd11f22c44e55fe2fc4c212df64e58af986
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-28 17:59:39 +1000

    Added State

--- sass/enjoyable-programming/menu-options/src/Components/Container/Container.jsx
+++ sass/enjoyable-programming/menu-options/src/Components/Container/Container.jsx
@@ -2,8 +2,63 @@ import React, { Component } from 'react';
 import './Container.scss';
 
 class ContainerComponent extends Component {
+  state = {
+    categories: [
+      {
+        name: 'Beer',
+        img: beer,
+        options: ['IPA', 'Brown Ale', 'Porter', 'Stout'],
+      },
+      {
+        name: 'Burger',
+        img: burger,
+        options: ['Cheese', 'Tex-Mex', 'Aloha', 'Portobello'],
+      },
+      {
+        name: 'Cereal',
+        img: cereal,
+        options: ['Oats', 'Wheet', 'Rice', 'Malze'],
+      },
+      {
+        name: 'Eggs',
+        img: eggs,
+        options: ['Boiled', 'Scrambled', 'Omlette', 'Over'],
+      },
+      {
+        name: 'Hotdog',
+        img: hotdog,
+        options: ['Chicago', 'Corney', 'Polish', 'Italian'],
+      },
+      {
+        name: 'Meat',
+        img: meat,
+        options: ['Duck', 'Rabbit', 'Goose', 'Turkey'],
+      },
+      {
+        name: 'Pizza',
+        img: pizza,
+        options: ['Sicillian', 'Neapolitan', 'Greek', 'California'],
+      },
+      {
+        name: 'Sandwich',
+        img: sandwich,
+        options: ['French', 'Cheese', 'Croque', 'Club'],
+      },
+    ],
+  };
+
   render() {
-    return <div>New Container</div>;
+    return (
+      <section className="menu-container">
+        <div className="bars"></div>
+        <section className="top">Categories</section>
+        <section className="bottom">
+          {this.state.categories.map((category) => {
+            return <div> </div>;
+          })}
+        </section>
+      </section>
+    );
   }
 }
 
