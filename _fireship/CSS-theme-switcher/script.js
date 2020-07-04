177a4785a1595c4b8eafdcb9c21f3f6ea5958677
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-04 18:13:51 +1000

    Updated script.js

--- _fireship/CSS-theme-switcher/script.js
+++ _fireship/CSS-theme-switcher/script.js
@@ -4,13 +4,24 @@ const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
+// Apply The Cached Theme On Reload
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add('theme');
+  isSolar && body.classList.add('solar');
+}
+
 // Button Event Handlers
 darkButton.onclick = () => {
   body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
 };
 
 lightButton.onclick = () => {
   body.classList.replace('dark', 'light');
+  localStorage.setItem('theme', 'light');
 };
 
 solarButton.onclick = () => {
@@ -20,11 +31,15 @@ solarButton.onclick = () => {
       --bg-solar: var(--yellow)
     `;
     solarButton.innerText = 'solarize';
+
+    localStorage.removeItem('isSolar');
   } else {
     solarButton.style.cssText = `
     --bg-solar: white
     `;
     body.classList.add('solar');
     solarButton.innerText = 'normalize';
+
+    localStorage.setItem('isSolar', true);
   }
 };
