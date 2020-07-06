ca85ec65b3e684e74e300c8700362efdee4afbc4
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-06 22:36:17 +1000

    Updated react-test-tutorial

--- test/reactjs-test-tutorial/src/App.js
+++ test/reactjs-test-tutorial/src/App.js
@@ -6,19 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ticket name="Super Bowl Tickets" />
+      <Footer />
     </div>
   );
 }
