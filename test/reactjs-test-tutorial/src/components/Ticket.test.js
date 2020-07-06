b754fb16897af12a69b63194bca17c691e852d55
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-06 23:22:59 +1000

    Updated react-test-tutorial

--- test/reactjs-test-tutorial/src/components/Ticket.test.js
+++ test/reactjs-test-tutorial/src/components/Ticket.test.js
@@ -5,5 +5,14 @@ import { Ticket } from './Ticket';
 it('should increment total when clicking button', () => {
   const wrapper = shallow(<Ticket />);
   const total1 = wrapper.find('h2.total').text();
+
   expect(total1).toBe('0');
+
+  const button = wrapper.find('button');
+
+  button.simulate('click');
+
+  const total2 = wrapper.find('h2.total').text();
+
+  expect(total2).toBe('1');
 });
