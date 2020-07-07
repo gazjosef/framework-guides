c6bb5bb846000169ead92cec950129ee143f5f87
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-07 13:49:31 +1000

    Updated react-test-tutorial

--- test/reactjs-test-tutorial/src/App.test.js
+++ test/reactjs-test-tutorial/src/App.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { Ticket } from './components/Ticket';
 import { Footer } from './components/Footer';
 
 it('should render Ticket and Footer', () => {
   const wrapper = shallow(<App />);
+  const ticket = wrapper.find(Ticket);
   const footer = wrapper.find(Footer);
 
+  expect(ticket.exists()).toBe(true);
   expect(footer.exists()).toBe(true);
 });
