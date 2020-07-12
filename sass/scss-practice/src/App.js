a70821190b16e8a7110bf9c76c2f9120dc36cbf4
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-12 22:11:54 +1000

    Updated Sass tutorial

--- sass/scss-practice/src/App.js
+++ sass/scss-practice/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button className="btn">This is my button</button>
+    </div>
+  );
 }
 
 export default App;
