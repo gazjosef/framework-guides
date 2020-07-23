3b4235563902c209428f049ebd29d017c156eeba
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-23 21:30:04 +1000

    Updated Tutorial

--- _traversyMedia/github-jobs-react-project/src/App.js
+++ _traversyMedia/github-jobs-react-project/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const { jobs, loading, error } = useFetchJobs();
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing</h1>}
+    </Container>
+  );
 }
 
 export default App;
