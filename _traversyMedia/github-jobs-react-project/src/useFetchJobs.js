03ddeee1a10f6a80a0382969833ecf4a419239b1
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-23 23:49:46 +1000

    Updated Tutorial

--- _traversyMedia/github-jobs-react-project/src/useFetchJobs.js
+++ _traversyMedia/github-jobs-react-project/src/useFetchJobs.js
@@ -1,25 +1,33 @@
 import { useReducer } from 'react';
+import axios from 'axios';
 
 const ACTIONS = {
   MAKE_REQUEST: 'make_request',
   GET_DATA: 'get-data',
-  ERROR: 'error'
-}
+  ERROR: 'error',
+};
 
 function reducer(state, action) {
-  switch(action.type) {
+  switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
-      
+      return { loading: true, jobs: [] };
     case ACTIONS.GET_DATA:
-      
+      return { ...state, loading: false, jobs: action.payload.jobs };
     case ACTIONS.ERROR:
-  } 
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
 }
 
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
-  dispatch({type: "Hello", payload: { x = 3}})
   return {
     jobs: [],
     loading: false,
