6ecdd5e73f48e07576f83a9baebe5a9f6bc3984a
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-23 23:59:57 +1000

    Updated Tutorial

--- _traversyMedia/github-jobs-react-project/src/useFetchJobs.js
+++ _traversyMedia/github-jobs-react-project/src/useFetchJobs.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import axios from 'axios';
 
 const ACTIONS = {
@@ -6,6 +6,8 @@ const ACTIONS = {
   GET_DATA: 'get-data',
   ERROR: 'error',
 };
+
+const BASE_URL = 'https://jobs.github.com/positions.json';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -28,9 +30,19 @@ function reducer(state, action) {
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
-  return {
-    jobs: [],
-    loading: false,
-    error: false,
-  };
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+  }, [params, page]);
+
+  return state;
 }
