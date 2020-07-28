a017a3c5e98dcd3987f3f3392f78980717a3a06a
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-28 21:37:08 +1000

    Updated Project

--- _traversyMedia/github-jobs-react-project/src/App.js
+++ _traversyMedia/github-jobs-react-project/src/App.js
@@ -3,6 +3,7 @@ import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 
 import Job from './Job';
+import JobsPagination from './JobsPagination';
 
 function App() {
   const [params, setParams] = useState({});
@@ -12,11 +13,13 @@ function App() {
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job} />;
       })}
+      <JobsPagination page={page} setPage={setPage} />
     </Container>
   );
 }
