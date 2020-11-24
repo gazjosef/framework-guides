a244c75c610b00ba7e39b9be875c743ae457f4ea
Gareth Hind <garethjhind@gmail.com>
2020-11-24 13:07:23 +1100

    Python lists.py

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -16,6 +16,37 @@ age = 37
 # print("My name is {name} and I am {age}".format(name=name, age=age))
 
 # F-Strings (only in 3.6+)
-print(f'My name is {name} and I am {age}')
+# print(f'My name is {name} and I am {age}')
 
 # String Methods
+s = "hello there world"
+
+# Capitalize first letter
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lower
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get length
+print(len(s))
+
+# Replace
+print(s.replace("world", "everyone"))
+
+# Count
+sub = "h"
+print(s.count(sub))
+
+# Starts with
+# End with
+# Split into a list
+# Find position
+# Is all alphanumeric?
+# Is all alphabetic?
+# Is all numeric?
