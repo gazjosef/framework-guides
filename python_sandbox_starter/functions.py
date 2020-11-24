fd0e7cdb422a4ffc39cb567deb7a2d4bd59ff8ae
Gareth Hind <garethjhind@gmail.com>
2020-11-24 21:29:03 +1100

    Python functions.py

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -28,3 +28,8 @@ print(new_num)
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
+getSum = lambda num1, num2 : num1 + num2
+print(getSum(9,2))
+
+addOneToNum = lambda num : num + 1
+print(addOneToNum(5))
