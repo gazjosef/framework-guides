8123cacdff375bb8e0ec4ec5d7674e126d936280
Gareth Hind <garethjhind@gmail.com>
2020-11-24 22:47:08 +1100

    Python conditionals.py

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,11 +1,32 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+x = 6
+y = 10
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+# Simple if
+if x == y:
+    print(f"{x} is equal to {y}")
 
+# If/else
+# if x > y:
+#     print(f"{x} is greater than {y}")
+# else:
+#     print(f"{x} is less than {y}")
 
+# Elif
+# if x > y:
+#     print(f"{x} is greater than {y}")
+# elif x==y:
+#     print(f"{x} is equal to {y}")
+# else:
+#     print(f"{x} is less than {y}")
+
+# Nested if
+if x > 2:
+    if x <= 10:
+        print(f"{x} is less than 2 and greater than 10")
 # Logical operators (and, or, not) - Used to combine conditional statements
 
 
