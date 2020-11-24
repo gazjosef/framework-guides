d295ef2727a0e844d0d2d25d546674ceac692787
Gareth Hind <garethjhind@gmail.com>
2020-11-24 12:20:58 +1100

    Python strings.py

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,15 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = "Brad"
+age = 37
+
+# Concatenate
+print("Hello I am " + name + " and I am " + str(age))
 
 # String Formatting
+
+# Arguments by position
+# print("{},{},{}".format("a","b","c"))
+print("{1},{2},{0}".format("a","b","c"))
 
 # String Methods
