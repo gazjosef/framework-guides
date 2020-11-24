e6df85b67a85e3e394307582658e827ea617d132
Gareth Hind <garethjhind@gmail.com>
2020-11-24 12:05:30 +1100

    Python Variables.py

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -14,4 +14,25 @@ VARIABLE RULES:
   - Can have numbers but can not start with one
 """
 
-print(1)
+# x = 1           # int
+# y = 2.5         # float
+# name = 'Brad'   # string
+# is_cool = True  # bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, "Brad", True)
+
+print(x, y, name, is_cool)
+
+# Basic math
+a = x + y
+print(a)
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+# Check type
+print(type(y))
+print(z)
