5fd127f6691850d870f492405f045ddbe019dba2
Gareth Hind <garethjhind@gmail.com>
2020-11-24 22:59:17 +1100

    Python conditionals.py

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -24,17 +24,45 @@ if x == y:
 #     print(f"{x} is less than {y}")
 
 # Nested if
-if x > 2:
-    if x <= 10:
-        print(f"{x} is less than 2 and greater than 10")
+# if x > 2:
+#     if x <= 10:
+#         print(f"{x} is less than 2 and greater than 10")
+
 # Logical operators (and, or, not) - Used to combine conditional statements
 
+# and
+# if x > 2 and x <= 10:
+#     print(f"{x} is less than 2 and greater than 10")
+
+# or
+# if x > 2 or x <= 10:
+#     print(f"{x} is less than 2 or greater than 10")
+
+# not
+# if not (x == y):
+#     print(f"{x} is not equal to {y}")
 
 
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
 
+numbers = [1,2,3,4,5]
 
+# in
+# if x in numbers:
+#     print(x in numbers)
+
+# not in
+# if x not in numbers:
+#     print(x in numbers)
 
 
 # Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+# is
+if x is y:
+    print(x is y)
+
+# is not
+if x is not y:
+    print(x is not y)
