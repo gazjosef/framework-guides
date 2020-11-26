399822729b97d0a9c7ce9d1f1205137e87c0e49f
Gareth Hind <garethjhind@gmail.com>
2020-11-26 21:17:05 +1100

    python classes.py

--- python_sandbox_starter/classes.py
+++ python_sandbox_starter/classes.py
@@ -8,11 +8,41 @@ class User:
         self.email = email
         self.age = age
     
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}'
+
+    def has_birthday(self):
+        self.age += 1
+
+# Customer class
+class Customer(User):
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and I owe a balance of {self.balance}'
+
+
 # Init user object
 brad = User("Brad Traversy", "brad@gmail.com", 37)
 janet = User("Janet Williams", "janet@gmail.com", 27)
 
 #Edit property
-brad.age =38
+brad.age = 38
 
-print(brad.age)
+janet.has_birthday()
+
+print(janet.greeting())
+
+# Init customer
+john = Customer("John Doe", "john@gmail.com", 40)
+
+john.set_balance(500)
+
+print(john.greeting())
