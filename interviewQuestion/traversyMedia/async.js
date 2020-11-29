ee9c5c2c38ea955fc17e5c7a119a2b1aecc0d79a
Gareth Hind <garethjhind@gmail.com>
2020-11-29 21:45:23 +1100

    Updated Folder

--- interviewQuestion/traversyMedia/async.js
+++
@@ -1,5 +0,0 @@
-// Async Data
-/*
-- When something is going on but you don't want to wait until that task is finished to continue programmming
-- Important to javascript because of server requests
-*/
