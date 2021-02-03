747fbbc7ce721583af28d4ed5a0264b20b92cd35
Gareth Hind <garethjhind@gmail.com>
2021-02-03 22:17:21 +1100

    Added react-testing-library

---
+++ react-testing-library/techsith/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import { Button } from "./components/button/Button"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button label="Click me please" className="button-style"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
