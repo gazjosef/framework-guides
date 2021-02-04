efeb2f14cccdb09073af6227fc9ae0fbcfd4d86c
Gareth Hind <garethjhind@gmail.com>
2021-02-04 21:47:12 +1100

    Updated framework

--- react-testing-library/techsith/src/components/button/__test__/button.test.js
+++ react-testing-library/techsith/src/components/button/__test__/button.test.js
@@ -1,2 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
+import { Button } from "./../Button";
+
+// import { render } from "@testing-library/react"
+
+it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Button></Button>, div)
+})
