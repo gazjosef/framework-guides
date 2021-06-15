3c9ae8356402b9682bced0a83ac5b4bcd83f9078
Gareth Hind <garethjhind@gmail.com>
2021-06-15 22:19:50 +1000

    Fixed CSS Bug

--- _tutorials/_fireship/facebook/src/App.js
+++ _tutorials/_fireship/facebook/src/App.js
@@ -1,16 +1,81 @@
-import React from 'react'
+import { ReactComponent as PlusIcon } from './icons/plus.svg'
+import { ReactComponent as BellIcon } from './icons/bell.svg'
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg'
+import { ReactComponent as CaretIcon } from './icons/caret.svg'
+import { ReactComponent as CogIcon } from './icons/cog.svg'
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg'
+// import { ReactComponent as ArrowIcon } from './icons/arrow.svg'
+// import { ReactComponent as BoltIcon } from './icons/bolt.svg'
+
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Navbar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>
+
+        <DropdownMenu></DropdownMenu>
+
+      </NavItem>
+    </Navbar>
   );
 }
 
-function Navbar() {
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <a href="/#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+
+        { props.children }
+
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    )
+  }
+
+   return (
+     <div className="dropdown">
+       <DropdownItem>My Profile</DropdownItem>
+       <DropdownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}
+       >
+         Settings
+       </DropdownItem>
+     </div>
+   )
+}
+
+function Navbar(props) {
   return (
-    <nav>
-      <ul></ul>
+    <nav className="navbar">
+      <ul className="navbar-nav">
+
+        {props.children}
+
+      </ul>
     </nav>
+  )
+}
+
+function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="/#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
   )
 }
 
