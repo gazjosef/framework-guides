dcf6da7b9a247688ae03dc3d1063eaf873c3fc2c
Gareth Hind <garethjhind@gmail.com>
2021-06-15 22:30:57 +1000

    Installed react-transition-group

--- _tutorials/_fireship/facebook/src/App.js
+++ _tutorials/_fireship/facebook/src/App.js
@@ -1,13 +1,14 @@
-import { ReactComponent as PlusIcon } from './icons/plus.svg'
-import { ReactComponent as BellIcon } from './icons/bell.svg'
-import { ReactComponent as MessengerIcon } from './icons/messenger.svg'
-import { ReactComponent as CaretIcon } from './icons/caret.svg'
-import { ReactComponent as CogIcon } from './icons/cog.svg'
-import { ReactComponent as ChevronIcon } from './icons/chevron.svg'
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
 // import { ReactComponent as ArrowIcon } from './icons/arrow.svg'
 // import { ReactComponent as BoltIcon } from './icons/bolt.svg'
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
   return (
@@ -17,55 +18,46 @@ function App() {
       <NavItem icon={<MessengerIcon />} />
 
       <NavItem icon={<CaretIcon />}>
-
         <DropdownMenu></DropdownMenu>
-
       </NavItem>
     </Navbar>
   );
 }
 
 function DropdownMenu() {
+  const [] = useState("main");
 
   function DropdownItem(props) {
     return (
       <a href="/#" className="menu-item">
         <span className="icon-button">{props.leftIcon}</span>
 
-        { props.children }
+        {props.children}
 
         <span className="icon-right">{props.rightIcon}</span>
       </a>
-    )
+    );
   }
 
-   return (
-     <div className="dropdown">
-       <DropdownItem>My Profile</DropdownItem>
-       <DropdownItem
-        leftIcon={<CogIcon />}
-        rightIcon={<ChevronIcon />}
-       >
-         Settings
-       </DropdownItem>
-     </div>
-   )
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />}>
+        Settings
+      </DropdownItem>
+    </div>
+  );
 }
 
 function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">
-
-        {props.children}
-
-      </ul>
+      <ul className="navbar-nav">{props.children}</ul>
     </nav>
-  )
+  );
 }
 
 function NavItem(props) {
-
   const [open, setOpen] = useState(false);
 
   return (
@@ -76,7 +68,7 @@ function NavItem(props) {
 
       {open && props.children}
     </li>
-  )
+  );
 }
 
 export default App;
