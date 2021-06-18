7bf86ae0bc3a2aa9009f778227ccd8369f425daf
Gareth Hind <garethjhind@gmail.com>
2021-06-18 11:07:52 +1000

    Updated Tutorial

--- _tutorials/_brianDesign/react-dropdown-navbar/src/components/Navbar/Navbar.js
+++ _tutorials/_brianDesign/react-dropdown-navbar/src/components/Navbar/Navbar.js
@@ -4,13 +4,39 @@ import { Dropdown } from "../Dropdown/Dropdown";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+import { FaTimes, FaBars, FaCaretDown } from "react-icons/fa";
+
 export const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="navbar-logo">
           EPIC
         </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Services <FaCaretDown />
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   );
