951787ca3839080fa0aa8d8006b2f086077b9642
Gareth Hind <garethjhind@gmail.com>
2021-09-15 16:29:54 +1000

    Custom React Hooks - useArray

--- react-hooks/custom-hooks/src/App.js
+++ react-hooks/custom-hooks/src/App.js
@@ -1,15 +1,15 @@
 // import ToggleComponent from "./1-useToggle/ToggleComponent";
 // import TimeOutComponent from "./2-useTimeOut/TimeOutComponent";
 // import DebounceComponent from "./3-useDebounce/DebounceComponent";
-import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent";
-// import ArrayComponent from "./5-useArray/ArrayComponent";
+// import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent";
+import ArrayComponent from "./5-useArray/ArrayComponent";
 
 function App() {
   // return <ToggleComponent />;
   // return <TimeOutComponent />;
   // return <DebounceComponent />;
-  return <UpdateEffectComponent />;
-  // return <ArrayComponent />;
+  // return <UpdateEffectComponent />;
+  return <ArrayComponent />;
 }
 
 export default App;
