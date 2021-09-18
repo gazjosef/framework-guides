fe10cfd29a7d7da5ae9b68c3f18eef3e779e8cab
Gareth Hind <garethjhind@gmail.com>
2021-09-18 15:41:29 +1000

    Added usePrevious

---
+++ react-hooks/custom-hooks/src/7-useStateWithHistory/useStateWithHistory.js
@@ -0,0 +1,12 @@
+import { useCallback, useRef, useState } from "react";
+
+export default function useStateWithHistory(
+  defaultValue,
+  { capacity = 10 } = {}
+) {
+  return (
+    <div>
+      <h1>useStateWithHistory</h1>
+    </div>
+  );
+}
--- react-hooks/custom-hooks/src/App.js
+++ react-hooks/custom-hooks/src/App.js
@@ -2,14 +2,18 @@
 // import TimeOutComponent from "./2-useTimeOut/TimeOutComponent";
 // import DebounceComponent from "./3-useDebounce/DebounceComponent";
 // import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent";
-import ArrayComponent from "./5-useArray/ArrayComponent";
+// import ArrayComponent from "./5-useArray/ArrayComponent";
+// import PreviousComponent from "./6-usePrevious/PreviousComponent";
+import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistoryComponent";
 
 function App() {
   // return <ToggleComponent />;
   // return <TimeOutComponent />;
   // return <DebounceComponent />;
   // return <UpdateEffectComponent />;
-  return <ArrayComponent />;
+  // return <ArrayComponent />;
+  // return <PreviousComponent />;
+  return <StateWithHistoryComponent />;
 }
 
 export default App;
