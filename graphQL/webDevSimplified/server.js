948dc0a8f1fefc13f2c3f704a2053fd303278a5a
Gareth Hind <garethjhind@gmail.com>
2022-01-05 13:18:16 +1100

    Updated GraphQL Project

--- graphQL/webDevSimplified/server.js
+++ graphQL/webDevSimplified/server.js
@@ -1,3 +1,22 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
-app.listen(5000, () => console.log("Server Running"));
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Hello World",
+    fields: () => ({
+      message: { type: GraphQLString, resolve: () => "Hello World" },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(5001, () => console.log("Server Running"));
