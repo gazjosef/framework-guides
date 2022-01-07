9b41b6526edc849b57f2992059018f0607119cd5
Gareth Hind <garethjhind@gmail.com>
2022-01-07 20:47:16 +1100

    Updated Typescript Tutorial

--- typescript/simple-typescript/src/index.ts
+++ typescript/simple-typescript/src/index.ts
@@ -5,3 +5,22 @@ const b = 2;
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
 mBy2(2);
+
+// * TYPES
+
+// Null & Undefinedx
+
+let n: null = null;
+let u: undefined = undefined;
+
+let someNumber: number = null;
+
+function upperFirstLetter(str: string | null) {
+  if (str) {
+    return str[0].toUpperCase() + str.substring(1);
+  }
+}
+
+console.log(upperFirstLetter("hello"));
+
+upperFirstLetter(null);
