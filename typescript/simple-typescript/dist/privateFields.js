fb785ef2a74f5de78191383569f09581380f1692
Gareth Hind <garethjhind@gmail.com>
2022-01-11 22:42:51 +1100

    Updated simple & Added react-plus

---
+++ typescript/simple-typescript/dist/privateFields.js
@@ -0,0 +1,39 @@
+"use strict";
+/*
+class Robot {
+  #name: string;
+
+  private somePrivateField = "It is private!!!";
+
+  constructor(name: string) {
+    this.#name = name;
+  }
+
+  getName() {
+    return this.#name;
+  }
+}
+
+class AdvancedRobot extends Robot {
+  #name: string;
+
+  private somePrivateField = "OVERRIDDEN PRIVATE FIELD";
+
+  constructor(name: string) {
+    super(name);
+    this.#name = `Advanced ${name}`;
+  }
+
+  getAdvancedRobotName() {
+    return this.#name;
+  }
+}
+
+const robot = new AdvancedRobot("Jackie");
+
+console.log("private", robot.somePrivateField);
+
+console.log("parent name", robot.getName());
+console.log("subclass name", robot.getAdvancedRobotName());
+
+*/
