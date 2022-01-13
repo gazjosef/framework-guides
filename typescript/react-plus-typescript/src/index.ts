b7d990409796901e4af4d41a775dd7576a426f16
Gareth Hind <garethjhind@gmail.com>
2022-01-13 22:05:28 +1100

    Updated react-plus-ts Project

--- typescript/react-plus-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-function log(str: string) {
-  console.log(str);
-}
-
-log("Hello World!!!");
-
-class A {
-  greating = "Hello World!";
-}
--- typescript/react-plus-typescript/webpack.config.js
+++ typescript/react-plus-typescript/webpack.config.js
@@ -1,7 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
+  devtool: "eval-source-map",
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
   },
@@ -12,6 +14,13 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { modules: true } },
+        ],
+      },
     ],
   },
   plugins: [
@@ -19,5 +28,6 @@ module.exports = {
       template: "./src/index.html",
       filename: "index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
