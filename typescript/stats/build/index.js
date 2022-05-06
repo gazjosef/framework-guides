d88ca548d5a864aab029e8c577f03648a8d9a32f
Gareth Hind <garethjhind@gmail.com>
2022-05-07 00:14:25 +1000

    Updated Stats

--- typescript/stats/build/index.js
+++ typescript/stats/build/index.js
@@ -1,31 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default
-    .readFileSync("football.csv", {
-    encoding: "utf-8",
-})
-    .split("\n")
-    .map((row) => {
-    return row.split(",");
-});
-// enum - enumeration
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
-let manUnitedWins = 0;
-for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+const MatchReader_1 = require("./MatchReader");
+const Summary_1 = require("./Summary");
+const matchReader = MatchReader_1.MatchReader.fromCsv("football.csv");
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport("Man United");
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
--- typescript/stats/src/index.ts
+++ typescript/stats/src/index.ts
@@ -1,29 +1,8 @@
-import fs from "fs";
+import { MatchReader } from "./MatchReader";
+import { Summary } from "./Summary";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
-// enum - enumeration
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
-
-let manUnitedWins = 0;
-
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
