2a484c2e292b434937ee62b8db6e95301bfb29fd
Gareth Hind <garethjhind@gmail.com>
2022-05-15 20:47:45 +1000

    Scroll Trigger Tutorial 6

--- gsap/scroll-trigger/app.js
+++ gsap/scroll-trigger/app.js
@@ -1,22 +1,22 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".square", {
-  //   x: 1000,
-  duration: 8,
-  scrollTrigger: {
-    trigger: ".square2",
-    start: "top 80%",
-    end: "top 30%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    pin: ".square",
-    pinSpacing: true,
-    // ^ OnEnter, onLeave, OnEnterBack, onLeaveBack
-    // play pause reverse restart reset complete none
-    markers: {
-      startColor: "purple",
-      endColor: "fuchsia",
-      fontSize: "3rem",
-    },
-  },
-});
+// gsap.to(".square", {
+//   //   x: 1000,
+//   duration: 8,
+//   scrollTrigger: {
+//     trigger: ".square2",
+//     start: "top 80%",
+//     end: "top 30%",
+//     scrub: 4,
+//     toggleActions: "restart none none none",
+//     pin: ".square",
+//     pinSpacing: true,
+//     // ^ OnEnter, onLeave, OnEnterBack, onLeaveBack
+//     // play pause reverse restart reset complete none
+//     markers: {
+//       startColor: "purple",
+//       endColor: "fuchsia",
+//       fontSize: "3rem",
+//     },
+//   },
+// });
