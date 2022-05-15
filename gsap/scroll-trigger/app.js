dcdae94e6e3d0a825ccb644917df907e828fef2d
Gareth Hind <garethjhind@gmail.com>
2022-05-15 11:23:14 +1000

    Updated Scroll Trigger

--- gsap/scroll-trigger/app.js
+++ gsap/scroll-trigger/app.js
@@ -1,14 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
-  x: 1000,
+  //   x: 1000,
   duration: 8,
   scrollTrigger: {
-    trigger: ".square",
+    trigger: ".square2",
     start: "top 80%",
     end: "top 30%",
     scrub: 4,
     toggleActions: "restart none none none",
+    pin: ".square",
+    pinSpacing: true,
     // ^ OnEnter, onLeave, OnEnterBack, onLeaveBack
     // play pause reverse restart reset complete none
     markers: {
