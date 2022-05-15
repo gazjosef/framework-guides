8320f62ebea5006fa8a0ec82f429e1134079a1fd
Gareth Hind <garethjhind@gmail.com>
2022-05-15 21:07:29 +1000

    Scroll Trigger Tutorial 6

--- gsap/scroll-trigger/app.js
+++ gsap/scroll-trigger/app.js
@@ -1,22 +1,19 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".square", {
-//   //   x: 1000,
-//   duration: 8,
-//   scrollTrigger: {
-//     trigger: ".square2",
-//     start: "top 80%",
-//     end: "top 30%",
-//     scrub: 4,
-//     toggleActions: "restart none none none",
-//     pin: ".square",
-//     pinSpacing: true,
-//     // ^ OnEnter, onLeave, OnEnterBack, onLeaveBack
-//     // play pause reverse restart reset complete none
-//     markers: {
-//       startColor: "purple",
-//       endColor: "fuchsia",
-//       fontSize: "3rem",
-//     },
-//   },
-// });
+// gsap.to(".box", { x: 500, duration: 5 });
+// gsap.to(".box", { y: 200, duration: 3, delay: 2 });
+// gsap.to(".box", { x: 0, duration: 2, delay: 5 });
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".box",
+    markers: true,
+    start: "top 80%",
+    end: "end 30%",
+    scrub: 1,
+  },
+});
+
+tl.to(".box", { x: 500, duration: 5 })
+  .to(".box", { y: 200, duration: 3 })
+  .to(".box", { x: 0, duration: 2 });
