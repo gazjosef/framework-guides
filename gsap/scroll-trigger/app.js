3e81a4d1b64a0518b0f602878d5e4f2bda778c6a
Gareth Hind <garethjhind@gmail.com>
2022-05-16 09:26:24 +1000

    Scroll Trigger Tutorial 7

--- gsap/scroll-trigger/app.js
+++ gsap/scroll-trigger/app.js
@@ -1,8 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
-  //   markers: "true",
-  start: "top 6%",
-  trigger: ".panel",
-  toggleClass: { targets: "nav", className: "nav-active" },
+  markers: "true",
+  start: "top 80%",
+  end: "top 50%",
+  trigger: ".box",
+  onUpdate: (self) => console.log(self),
+  //   onEnter: () => console.log("enter!"),
+  //   onLeave: () => console.log("leave!"),
+  //   onEnterBack: () => console.log("enterBack!"),
+  //   onLeaveBack: () => console.log("leaveBack!"),
 });
