c6a15283b387817abcbeb59cfcf3e1290132b82e
Gareth Hind <garethjhind@gmail.com>
2022-05-16 09:08:34 +1000

    Scroll Trigger Tutorial 7

--- gsap/scroll-trigger/app.js
+++ gsap/scroll-trigger/app.js
@@ -1,19 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".box", { x: 500, duration: 5 });
-// gsap.to(".box", { y: 200, duration: 3, delay: 2 });
-// gsap.to(".box", { x: 0, duration: 2, delay: 5 });
-
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".box",
-    markers: true,
-    start: "top 80%",
-    end: "end 30%",
-    scrub: 1,
-  },
+ScrollTrigger.create({
+  trigger: ".box",
+  start: "top 80%",
+  end: "top 50%",
+  markers: "true",
 });
-
-tl.to(".box", { x: 500, duration: 5 })
-  .to(".box", { y: 200, duration: 3 })
-  .to(".box", { x: 0, duration: 2 });
