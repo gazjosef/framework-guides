2328056bf074a908dae9075423916b0068ae54e2
Gareth Hind <garethjhind@gmail.com>
2022-06-27 22:31:38 +1000

    Updated react-testing

---
+++ react-testing-library/react-testing-starter-main/src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on initial render, the pay button is disabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  //   screen.debug();
+  //   screen.getByRole("");
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+});
