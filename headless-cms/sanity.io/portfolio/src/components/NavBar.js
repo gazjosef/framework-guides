7da5705b4c7ed361f5c6a77fc63ef3357ca1f8d6
Gareth Hind <garethjhind@gmail.com>
2022-12-10 23:42:19 +1100

    Sanity.io - Post Component

--- headless-cms/sanity.io/portfolio/src/components/NavBar.js
+++ headless-cms/sanity.io/portfolio/src/components/NavBar.js
@@ -9,29 +9,29 @@ export default function NavBar() {
         <nav className="flex">
           <NavLink
             to="/"
-            activeClassName="text-white"
+            // activeClassName="text-white"
             className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
-            exact
+            exact="true"
           >
             Kapehe
           </NavLink>
           <NavLink
             to="/post"
-            activeClassName="text-red-100 bg-red-700"
+            // activeClassName="text-red-100 bg-red-700"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
           >
             Blog Post
           </NavLink>
           <NavLink
             to="/project"
-            activeClassName="text-red-100 bg-red-700"
+            // activeClassName="text-red-100 bg-red-700"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
-            activeClassName="text-red-100 bg-red-700"
+            // activeClassName="text-red-100 bg-red-700"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
           >
             About Me
