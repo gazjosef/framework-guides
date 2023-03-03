c5330967e32f283a3cc0b606dfbad3e40ca95b41
Gareth Hind <garethjhind@gmail.com>
2023-03-03 23:52:33 +1100

    Updated

---
+++ weather-app-new/components/iPhone.jsx
@@ -0,0 +1,22 @@
+// import WeatherApp from "@/components/WeatherApp/WeatherApp";
+
+export default function iPhone() {
+  return (
+    <div className="center">
+      <div className="mobile | w-[276px] h-[580px] bg-white rounded-3xl	border-solid border-2 box-border">
+        <div className="home">HOME</div>
+        <div className="inner">INNER</div>
+        <ul className="volume">
+          <li></li>
+          <li></li>
+        </ul>
+        <ul className="silent">
+          <li></li>
+        </ul>
+        <ul className="sleep">
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
