296be4846637198d5506ce0edde34dd42f8735f5
Gareth Hind <garethjhind@gmail.com>
2023-03-04 16:52:27 +1100

    Updated

---
+++ weather-app-new/components/WeatherApp.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { IconContext } from "react-icons";
+import { FaSearch } from "react-icons/fa";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDaySunnyOvercast,
+  WiNightPartlyCloudy,
+  WiCloud,
+  WiDayCloudy,
+  WiNightCloudy,
+  WiRain,
+  WiDayShowers,
+  WiNightShowers,
+  WiThunderstorm,
+  WiSnow,
+  WiFog,
+} from "react-icons/wi";
+
+import React from "react";
+
+const API_KEY = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
+
+const WeatherApp = () => {
+  return <div>WeatherApp</div>;
+};
+
+export default WeatherApp;
--- weather-app-new/components/iPhone.jsx
+++ weather-app-new/components/iPhone.jsx
@@ -1,26 +1,29 @@
-// import WeatherApp from "@/components/WeatherApp/WeatherApp";
+import WeatherApp from "@/components/WeatherApp";
 
 export default function iPhone() {
   return (
     <div className="center">
       <div className="mobile | bg-white h-[580px] w-[276px] border-solid border-2 box-border rounded-[30px] shadow-[inset_0_0_8px_rgba(0,0,0,0.3)] | relative">
-        <div className="screen | bg-white h-[455px] w-[256px] border-solid border-[1px] mt-24 mx-auto | relative"></div>
+        <div className="screen | bg-white h-[455px] w-[256px] border-solid border-[1px] mt-24 mx-auto | relative">
+          {/* WEATHER APP */}
+          <WeatherApp />
+        </div>
         <div
-          className="inner | bg-black h-1 w-10 box-border rounded-sm | absolute top-8 left-2/4 -translate-x-2/4 |
-        before:content-[''] before:bg-black before:h-2 before:w-2 before:rounded-[50%] | before:absolute before:top-[-2px] before:left-[-22px] | 
-        after:content-[''] after:bg-black after:h-1.5 after:w-1.5 after:rounded-[50%] | after:absolute after:top-[-2px] after:left-[-22px]"
+          className="inner | bg-black h-[4px] w-[40px] box-border rounded-sm | absolute top-[32px] left-2/4 -translate-x-2/4 |
+        before:content-[''] before:bg-black before:h-[8px] before:w-[8px] before:rounded-[50%] | before:absolute before:top-[-2px] before:left-[-22px] | 
+        after:content-[''] after:bg-black after:h-[6px] after:w-[6px] after:rounded-[50%] | after:absolute after:top-[-16px] after:left-2/4"
         ></div>
-        <ul className="volume">
-          <li></li>
-          <li></li>
+        <ul className="volume | absolute top-[90px] left-[-2px]">
+          <li className="bg-slate-400 h-[30px] w-[2px] mt-[10px] mx-0 rounded-tl-[4px] rounded-bl-[4px]"></li>
+          <li className="bg-slate-400 h-[30px] w-[2px] mt-[10px] mx-0 rounded-tl-[4px] rounded-bl-[4px]"></li>
         </ul>
-        <ul className="silent">
-          <li></li>
+        <ul className="silent | absolute top-[50px] left-[-2px]">
+          <li className="bg-slate-400 h-[20px] w-[2px] mt-[10px] mx-0 rounded-tl-[4px] rounded-bl-[4px]"></li>
         </ul>
-        <ul className="sleep">
-          <li></li>
+        <ul className="sleep | absolute top-[90px] right-[-2px]">
+          <li className="bg-slate-400 h-[30px] w-[2px] mt-[10px] mx-0 rounded-tl-[4px] rounded-bl-[4px]"></li>
         </ul>
-        <div className="home | bg-white h-10 w-10 border-solid border-2 border-slate-50 box-border rounded-[50%] shadow-[inset_0_0_7px_rgba(0,0,0,0.1)] | absolute bottom-3.5 left-2/4	-translate-x-2/4"></div>
+        <div className="home | bg-white h-[40px] w-[40px] border-[2px] border-solid border-slate-200 box-border rounded-[50%] shadow-[inset_0_0_7px_rgba(0,0,0,0.1)] | absolute bottom-[15px] left-2/4	-translate-x-2/4"></div>
       </div>
     </div>
   );
