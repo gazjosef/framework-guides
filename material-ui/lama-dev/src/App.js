284551b52a9f2eada497b4979d9681d51a8b6387
Gareth Hind <garethjhind@gmail.com>
2023-06-06 12:08:39 +1000

    Material UI - Buttons

--- material-ui/lama-dev/src/App.js
+++ material-ui/lama-dev/src/App.js
@@ -1,7 +1,30 @@
+import { Button } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<AddIcon />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
     </div>
   );
 }
