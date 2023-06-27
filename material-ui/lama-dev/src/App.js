eca6486c3e0026ca079b6e6a8bb530f86b97b951
Gareth Hind <garethjhind@gmail.com>
2023-06-27 21:03:33 +1000

    Material UI - Custom Theme

--- material-ui/lama-dev/src/App.js
+++ material-ui/lama-dev/src/App.js
@@ -3,8 +3,8 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import AddIcon from "@mui/icons-material/Add";
 
 function App() {
-  const BlueButton = styled(Button)({
-    backgroundColor: "skyblue",
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.otherColor.main,
     color: "#888",
     margin: 5,
     "&:hover": {
@@ -14,7 +14,7 @@ function App() {
       backgroundColor: "gray",
       color: "white",
     },
-  });
+  }));
 
   return (
     <div className="App">
@@ -22,7 +22,7 @@ function App() {
       <Button
         startIcon={<SettingsIcon />}
         variant="contained"
-        color="secondary"
+        color="otherColor"
         size="small"
       >
         Settings
