8f5ebe4077a2b81af75d6bd95800fff32b30133d
Gareth Hind <garethjhind@gmail.com>
2023-07-09 11:54:34 +1000

    ThreeJs: Install styled-components

--- threeJs/lama-dev/src/App.jsx
+++ threeJs/lama-dev/src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
 
+function App() {
   return (
     <div>
-      Hello world!
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
