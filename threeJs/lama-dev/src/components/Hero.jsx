a06e4a7e0ec8e9e2ee4d70a257ac7f7a72cb517f
Gareth Hind <garethjhind@gmail.com>
2023-07-15 20:52:12 +1000

    ThreeJs: Hero Section

--- threeJs/lama-dev/src/components/Hero.jsx
+++ threeJs/lama-dev/src/components/Hero.jsx
@@ -13,13 +13,86 @@ const Section = styled.div`
 const Container = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+const Title = styled.h1`
+  font-size: 74px;
+`;
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Line = styled.img`
+  height: 5px;
+`;
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+const Button = styled.button`
+  cursor: pointer;
+  background-color: #da4ea2;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+
+  color: white;
+  font-weight: 500;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  margin: auto;
+
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 `;
 
 const Hero = () => {
   return (
     <Section>
       <Navbar />
-      <Container>Hero</Container>
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What we Do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            we enjoy creating delightful, human-centered digital experiences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {/* 3d model */}
+          <Img src="./img/moon.png" />
+        </Right>
+      </Container>
     </Section>
   );
 };
