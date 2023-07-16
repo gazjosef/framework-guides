77d36853f60e4d61f62cba340105f2fbfd1af256
Gareth Hind <garethjhind@gmail.com>
2023-07-16 19:23:28 +1000

    ThreeJs: Added Styling to Contact

--- threeJs/lama-dev/src/components/Contact.jsx
+++ threeJs/lama-dev/src/components/Contact.jsx
@@ -5,7 +5,78 @@ const Section = styled.div`
   scroll-snap-align: center;
 `;
 
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  background-color: #da4ea2;
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+
+  color: white;
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 const Contact = () => {
-  return <Section>Contact</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <TextArea placeholder="Write your message" rows={10} />
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 };
 export default Contact;
