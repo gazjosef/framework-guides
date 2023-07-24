2bd2f1b3d69426d08cf0cea9d837e8c4e5935107
Gareth Hind <garethjhind@gmail.com>
2023-07-24 21:44:11 +1000

    ThreeJs: Tutorial

--- threeJs/lama-dev/src/App.jsx
+++ threeJs/lama-dev/src/App.jsx
@@ -3,6 +3,7 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Works from "./components/Works";
+import Test from "./components/Test";
 
 const Container = styled.div`
   background: url("./img/bg.jpeg");
@@ -26,6 +27,7 @@ function App() {
       <Who />
       <Works />
       <Contact />
+      <Test />
     </Container>
   );
 }
