0db92e122edfc4e4a243d5165c24fa3d1e153abe
Gareth Hind <garethjhind@gmail.com>
2023-07-26 20:12:25 +1000

    ThreeJs: Tutorial

--- threeJs/lama-dev/src/components/Test.jsx
+++ threeJs/lama-dev/src/components/Test.jsx
@@ -1,4 +1,9 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  RenderTexture,
+  Text,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 
@@ -14,9 +19,18 @@ const Test = () => {
       <Canvas>
         <OrbitControls enableZoom={false} />
         <ambientLight intensity={1} />
+        <directionalLight position={[3, 2, 1]} />
         <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="red" />
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial>
+            <RenderTexture attach="map">
+              <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+              <color attach="background" args={["pink"]} />
+              <Text fontSize={3} color="#555">
+                hello
+              </Text>
+            </RenderTexture>
+          </meshStandardMaterial>
         </mesh>
       </Canvas>
     </Container>
