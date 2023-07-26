4bf800bdd5f15c3d29e8f03bff4ea9e4938d58ab
Gareth Hind <garethjhind@gmail.com>
2023-07-26 20:17:24 +1000

    ThreeJs: Tutorial

---
+++ threeJs/lama-dev/src/components/Cube.jsx
@@ -0,0 +1,24 @@
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  RenderTexture,
+  Text,
+} from "@react-three/drei";
+
+const Cube = () => {
+  return (
+    <mesh>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial>
+        <RenderTexture attach="map">
+          <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+          <color attach="background" args={["#dc9dcd"]} />
+          <Text fontSize={1} color="#555">
+            hello
+          </Text>
+        </RenderTexture>
+      </meshStandardMaterial>
+    </mesh>
+  );
+};
+export default Cube;
--- threeJs/lama-dev/src/components/Test.jsx
+++ threeJs/lama-dev/src/components/Test.jsx
@@ -1,11 +1,7 @@
-import {
-  OrbitControls,
-  PerspectiveCamera,
-  RenderTexture,
-  Text,
-} from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
+import Cube from "./Cube";
 
 const Container = styled.div`
   height: 100vh;
@@ -20,18 +16,7 @@ const Test = () => {
         <OrbitControls enableZoom={false} />
         <ambientLight intensity={1} />
         <directionalLight position={[3, 2, 1]} />
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial>
-            <RenderTexture attach="map">
-              <PerspectiveCamera makeDefault position={[0, 0, 2]} />
-              <color attach="background" args={["pink"]} />
-              <Text fontSize={3} color="#555">
-                hello
-              </Text>
-            </RenderTexture>
-          </meshStandardMaterial>
-        </mesh>
+        <Cube />
       </Canvas>
     </Container>
   );
