3e445ba27b9576873d6f8438511152d9a7725f82
Gareth Hind <garethjhind@gmail.com>
2023-07-29 19:19:46 +1000

    ThreeJs: Tutorial

--- threeJs/lama-dev/src/components/Cube.jsx
+++ threeJs/lama-dev/src/components/Cube.jsx
@@ -1,6 +1,12 @@
 import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Cube = () => {
+  const textRef = useRef();
+  useFrame(
+    (state) => (textRef.current.position.x = Math.sin(state.clock.elapsedTime))
+  );
   return (
     <mesh>
       <boxGeometry args={[2, 2, 2]} />
@@ -8,7 +14,7 @@ const Cube = () => {
         <RenderTexture attach="map">
           <PerspectiveCamera makeDefault position={[0, 0, 2]} />
           <color attach="background" args={["#dc9dcd"]} />
-          <Text fontSize={1} color="#555">
+          <Text ref={textRef} fontSize={1} color="#555">
             hello
           </Text>
         </RenderTexture>
