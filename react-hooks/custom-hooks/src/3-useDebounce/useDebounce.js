656b75082bad441485f4b59a54397165fc2122c0
Gareth Hind <garethjhind@gmail.com>
2024-06-01 17:34:31 +1000

    Refactor: Rm Outdated Projects

--- react-hooks/custom-hooks/src/3-useDebounce/useDebounce.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import useTimeOut from "../2-useTimeOut/useTimeOut";
-
-export default function useDebounce(callback, delay, dependencies) {
-  const { reset, clear } = useTimeOut(callback, delay);
-
-  useEffect(reset, [...dependencies, reset]);
-  useEffect(clear, []);
-}
